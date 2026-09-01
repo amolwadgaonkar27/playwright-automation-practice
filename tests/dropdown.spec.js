1c199c54ebbdd0bc123ab8b85d198c8043b146ec
Amol Wadgaonkar <amol.wadgaonkar@acldigial.com>
2026-09-01 18:33:26 +0530

    Added TC059 to 61

--- tests/dropdown.spec.js
+++ tests/dropdown.spec.js
@@ -1,5 +1,4 @@
 const { test, expect } = require('../fixtures/baseFixture');
-const loadTestData = require('../utils/testDataLoader');
 
 test.describe('Dropdown Tests', () => {
     test.beforeEach(async ({ loginPage, dashboardPage }) => {
@@ -10,7 +9,7 @@ test.describe('Dropdown Tests', () => {
         await expect(dashboardPage.dashboardHeader).toHaveText('Dashboard');
     });
 
-test('TC-034 - Verify that User Management dropdown is clickable on Admin Page', async ({ dashboardPage, adminPage }) => {
+    test('TC-034 - Verify that User Management dropdown is clickable on Admin Page', async ({ dashboardPage, adminPage }) => {
         await dashboardPage.openAdmin();
 
         await expect(adminPage.adminHeader).toHaveText('Admin/User Management');
@@ -20,7 +19,7 @@ test('TC-034 - Verify that User Management dropdown is clickable on Admin Page',
         await expect(adminPage.userManagementDropdown).toBeVisible();
     });
 
-test('TC035 - Verify that Job dropdown is clickable on Admin Page', async ({ dashboardPage, adminPage }) => {
+    test('TC035 - Verify that Job dropdown is clickable on Admin Page', async ({ dashboardPage, adminPage }) => {
         await dashboardPage.openAdmin();
 
         await expect(adminPage.adminHeader).toHaveText('Admin/User Management');
@@ -30,7 +29,7 @@ test('TC035 - Verify that Job dropdown is clickable on Admin Page', async ({ das
         await expect(adminPage.jobDropdown).toBeVisible();
     });
 
-test('TC036 - Verify that Organization dropdown is clickable on Admin Page', async ({ dashboardPage, adminPage }) => {
+    test('TC036 - Verify that Organization dropdown is clickable on Admin Page', async ({ dashboardPage, adminPage }) => {
         await dashboardPage.openAdmin();
 
         await expect(adminPage.adminHeader).toHaveText('Admin/User Management');
@@ -40,7 +39,7 @@ test('TC036 - Verify that Organization dropdown is clickable on Admin Page', asy
         await expect(adminPage.organizationDropdown).toBeVisible();
     });
 
-test('TC037 - Verify that Qualifications dropdown is clickable on Admin Page', async ({ dashboardPage, adminPage }) => {
+    test('TC037 - Verify that Qualifications dropdown is clickable on Admin Page', async ({ dashboardPage, adminPage }) => {
         await dashboardPage.openAdmin();
 
         await expect(adminPage.adminHeader).toHaveText('Admin/User Management');
@@ -50,7 +49,7 @@ test('TC037 - Verify that Qualifications dropdown is clickable on Admin Page', a
         await expect(adminPage.qualificationsDropdown).toBeVisible();
     });
 
-test('TC038 - Verify that Configuration dropdown is clickable on Admin Page', async ({ dashboardPage, adminPage }) => {
+    test('TC038 - Verify that Configuration dropdown is clickable on Admin Page', async ({ dashboardPage, adminPage }) => {
         await dashboardPage.openAdmin();
 
         await expect(adminPage.adminHeader).toHaveText('Admin/User Management');
@@ -60,7 +59,7 @@ test('TC038 - Verify that Configuration dropdown is clickable on Admin Page', as
         await expect(adminPage.configurationDropdown).toBeVisible();
     });
 
-test('TC039 - Verify that Configuration dropdown is clickable on PIM Page', async ({ dashboardPage, pimPage }) => {
+    test('TC039 - Verify that Configuration dropdown is clickable on PIM Page', async ({ dashboardPage, pimPage }) => {
         await dashboardPage.openPIM();
 
         await expect(pimPage.pimHeader).toHaveText('PIM');
@@ -70,7 +69,7 @@ test('TC039 - Verify that Configuration dropdown is clickable on PIM Page', asyn
         await expect(pimPage.configurationDropdown).toBeVisible();
     });
 
-test('TC040 - Verify that Entitlements dropdown is clickable on Leave Page', async ({ dashboardPage, leavePage }) => {
+    test('TC040 - Verify that Entitlements dropdown is clickable on Leave Page', async ({ dashboardPage, leavePage }) => {
         await dashboardPage.openLeave();
 
         await expect(leavePage.leaveHeader).toHaveText('Leave');
@@ -80,7 +79,7 @@ test('TC040 - Verify that Entitlements dropdown is clickable on Leave Page', asy
         await expect(leavePage.entitlementsDropdown).toBeVisible();
     });
 
-test('TC041 - Verify that Reports dropdown is clickable on Leave page', async({ leavePage }) => {
+    test('TC041 - Verify that Reports dropdown is clickable on Leave page', async ({ leavePage }) => {
         await leavePage.openLeave();
 
         await leavePage.clickReportsDropdown();
@@ -88,7 +87,7 @@ test('TC041 - Verify that Reports dropdown is clickable on Leave page', async({ 
         await expect(leavePage.reportsDropdown).toBeVisible();
     });
 
-test('TC-042 - Verify that Configure dropdown is clickable on Leave page', async({ leavePage }) => {
+    test('TC-042 - Verify that Configure dropdown is clickable on Leave page', async ({ leavePage }) => {
         await leavePage.openLeave();
 
         await leavePage.clickConfigureDropdown();
@@ -96,7 +95,7 @@ test('TC-042 - Verify that Configure dropdown is clickable on Leave page', async
         await expect(leavePage.configureDropdown).toBeVisible();
     });
 
-test('TC043 - Verify that Timesheets dropdown is clickable on Time page', async({ timePage }) => {
+    test('TC043 - Verify that Timesheets dropdown is clickable on Time page', async ({ timePage }) => {
         await timePage.openTime();
 
         await timePage.clickTimesheetsDropdown();
@@ -104,7 +103,7 @@ test('TC043 - Verify that Timesheets dropdown is clickable on Time page', async(
         await expect(timePage.timesheetsDropdown).toBeVisible();
     });
 
-test('TC044 - Verify that Attendance dropdown is clickable on Time page', async({ timePage }) => {
+    test('TC044 - Verify that Attendance dropdown is clickable on Time page', async ({ timePage }) => {
         await timePage.openTime();
 
         await timePage.clickAttendanceDropdown();
@@ -112,7 +111,7 @@ test('TC044 - Verify that Attendance dropdown is clickable on Time page', async(
         await expect(timePage.attendanceDropdown).toBeVisible();
     });
 
-test('TC045 - Verify that Reports dropdown is clickable on Time page', async({ timePage }) => {
+    test('TC045 - Verify that Reports dropdown is clickable on Time page', async ({ timePage }) => {
         await timePage.openTime();
 
         await timePage.clickReportsDropdown();
@@ -120,7 +119,7 @@ test('TC045 - Verify that Reports dropdown is clickable on Time page', async({ t
         await expect(timePage.reportsDropdown).toBeVisible();
     });
 
-test('TC046 - Verify that Project Info dropdown is clickable on Time page', async({ timePage }) => {
+    test('TC046 - Verify that Project Info dropdown is clickable on Time page', async ({ timePage }) => {
         await timePage.openTime();
 
         await timePage.clickProjectInfoDropdown();
@@ -128,7 +127,7 @@ test('TC046 - Verify that Project Info dropdown is clickable on Time page', asyn
         await expect(timePage.projectInfoDropdown).toBeVisible();
     });
 
-test('TC047 - Verify that Configure dropdown is clickable on Performance page',async({ performancePage }) => {
+    test('TC047 - Verify that Configure dropdown is clickable on Performance page', async ({ performancePage }) => {
         await performancePage.openPerformance();
 
         await performancePage.clickConfigureDropdown();
@@ -136,21 +135,26 @@ test('TC047 - Verify that Configure dropdown is clickable on Performance page',a
         await expect(performancePage.configureDropdown).toBeVisible();
     });
 
-test('TC048 - Verify that Manage Reviews dropdown is clickable on Performance page', async({ performancePage }) => {
+    test('TC048 - Verify that Manage Reviews dropdown is clickable on Performance page', async ({ performancePage }) => {
         await performancePage.openPerformance();
 
         await performancePage.clickManageReviewsDropdown();
 
         await expect(performancePage.manageReviewsDropdown).toBeVisible();
-});
+    });
 
-test('TC049 - Verify that Configuration dropdown is clickable on Claim Page', async({ claimPage }) => {
+    test('TC049 - Verify that Configuration dropdown is clickable on Claim Page', async ({ claimPage }) => {
         await claimPage.openClaim();
 
         await claimPage.clickConfigurationDropdown();
 
         await expect(claimPage.configurationDropdown).toBeVisible();
 
-})
+    })
 
+    test('TC061 - Verify that Users option from dropdown is clickable on Admin Page', async ({ page }) => {
+        await page.getByRole('link', { name: 'Admin' }).click();
+        await page.getByLabel('Topbar Menu').getByText('User Management').click();
+        await page.getByRole('listitem').filter({ hasText: /^Users$/ }).click();
+    });
 });
--- tests/help.spec.js
+++ tests/help.spec.js
@@ -1,83 +1,88 @@
 const { test, expect } = require('../fixtures/baseFixture');
 
-test.describe('Dropdown Tests', () => {
-    test.beforeEach(async ({ loginPage, dashboardPage }) => {
-        await loginPage.goto();
+test.describe('Help Tests', () => {
+  test.beforeEach(async ({ loginPage, dashboardPage }) => {
+    await loginPage.goto();
+    await loginPage.login('Admin', 'admin123');
+    await expect(dashboardPage.dashboardHeader).toHaveText('Dashboard');
+  });
 
-        await loginPage.login('Admin', 'admin123');
+  test('TC050 - Verify that help button is clickable on Admin page', async ({ page }) => {
+    await page.getByRole('link', { name: 'Admin' }).click();
+    const page1Promise = page.waitForEvent('popup');
+    await page.getByTitle('Help').click();
+    const page1 = await page1Promise;
+  });
 
-        await expect(dashboardPage.dashboardHeader).toHaveText('Dashboard');
-    });
+  test('TC051 - Verify that help button is clickable on PIM page', async ({ page }) => {
+    await page.getByRole('link', { name: 'PIM' }).click();
+    const page1Promise = page.waitForEvent('popup');
+    await page.getByTitle('Help').click();
+    const page1 = await page1Promise;
+  });
 
-test('TC050 - Verify that help button is clickable on Admin page', async ({ page }) => {
-  await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
-  await page.getByRole('link', { name: 'Admin' }).click();
-  const page1Promise = page.waitForEvent('popup');
-  await page.getByTitle('Help').click();
-  const page1 = await page1Promise;
-});
+  test('TC052 - Verify that help button is clickable on Leave page', async ({ page }) => {
+    await page.getByRole('link', { name: 'Leave' }).click();
+    const page1Promise = page.waitForEvent('popup');
+    await page.getByTitle('Help').click();
+    const page1 = await page1Promise;
+  });
 
-test('TC051 - Verify that help button is clickable on PIM page', async ({ page }) => {
-  await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
-  await page.getByRole('link', { name: 'PIM' }).click();
-  const page1Promise = page.waitForEvent('popup');
-  await page.getByTitle('Help').click();
-  const page1 = await page1Promise;
-});
+  test('TC053 - Verify that help button is clickable on Time page', async ({ page }) => {
+    await page.getByRole('link', { name: 'Time' }).click();
+    const page1Promise = page.waitForEvent('popup');
+    await page.getByTitle('Help').click();
+    const page1 = await page1Promise;
+  });
 
-test('TC052 - Verify that help button is clickable on Leave page', async ({ page }) => {
-  await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
-  await page.getByRole('link', { name: 'Leave' }).click();
-  const page1Promise = page.waitForEvent('popup');
-  await page.getByTitle('Help').click();
-  const page1 = await page1Promise;
-});
+  test('TC054 - Verify that help button is clickable on Recruitment page', async ({ page }) => {
+    await page.getByRole('link', { name: 'Recruitment' }).click();
+    const page1Promise = page.waitForEvent('popup');
+    await page.getByTitle('Help').click();
+    const page1 = await page1Promise;
+  });
 
-test('TC053 - Verify that help button is clickable on Time page', async ({ page }) => {
-  await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
-  await page.getByRole('link', { name: 'Time' }).click();
-  const page1Promise = page.waitForEvent('popup');
-  await page.getByTitle('Help').click();
-  const page1 = await page1Promise;
-});
+  test('TC055 - Verify that help button is clickable on My Info page', async ({ page }) => {
+    await page.getByRole('link', { name: 'My Info' }).click();
+    const page1Promise = page.waitForEvent('popup');
+    await page.getByTitle('Help').click();
+    const page1 = await page1Promise;
+  });
 
-test('TC054 - Verify that help button is clickable on Recruitment page', async ({ page }) => {
-  await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
-  await page.getByRole('link', { name: 'Recruitment' }).click();
-  const page1Promise = page.waitForEvent('popup');
-  await page.getByTitle('Help').click();
-  const page1 = await page1Promise;
-});
+  test('TC056 - Verify that help button is clickable on Performance page', async ({ page }) => {
+    await page.getByRole('link', { name: 'Performance' }).click();
+    const page1Promise = page.waitForEvent('popup');
+    await page.getByTitle('Help').click();
+    const page1 = await page1Promise;
+  });
 
-test('TC055 - Verify that help button is clickable on My Info page', async ({ page }) => {
-  await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
-  await page.getByRole('link', { name: 'My Info' }).click();
-  const page1Promise = page.waitForEvent('popup');
-  await page.getByTitle('Help').click();
-  const page1 = await page1Promise;
-});
+  test('TC057 - Verify that help button is clickable on Dashboard page', async ({ page }) => {
+    await page.getByRole('link', { name: 'Dashboard' }).click();
+    const page1Promise = page.waitForEvent('popup');
+    await page.getByTitle('Help').click();
+    const page1 = await page1Promise;
+  });
 
-test('TC056 - Verify that help button is clickable on Performance page', async ({ page }) => {
-  await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
-  await page.getByRole('link', { name: 'Performance' }).click();
-  const page1Promise = page.waitForEvent('popup');
-  await page.getByTitle('Help').click();
-  const page1 = await page1Promise;
-});
+  test('TC058 - Verify that help button is clickable on Directory page', async ({ page }) => {
+    await page.getByRole('link', { name: 'Directory' }).click();
+    const page1Promise = page.waitForEvent('popup');
+    await page.getByTitle('Help').click();
+    const page1 = await page1Promise;
+  });
 
-test('TC057 - Verify that help button is clickable on Dashboard page', async ({ page }) => {
-  await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
-  await page.getByRole('link', { name: 'Dashboard' }).click();
-  const page1Promise = page.waitForEvent('popup');
-  await page.getByTitle('Help').click();
-  const page1 = await page1Promise;
-});
+  test('TC059 - Verify that help button is clickable on Claim page', async ({ page }) => {
+    await page.getByRole('link', { name: 'Claim' }).click();
+    const page1Promise = page.waitForEvent('popup');
+    await page.getByTitle('Help').click();
+    const page1 = await page1Promise;
+  });
 
-test('TC058 - Verify that help button is clickable on Directory page', async ({ page }) => {
-  await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
-  await page.getByRole('link', { name: 'Directory' }).click();
-  const page1Promise = page.waitForEvent('popup');
-  await page.getByTitle('Help').click();
-  const page1 = await page1Promise;
-});
+  test('TC060 - Verify that help button is clickable on Buzz page', async ({ page }) => {
+    await page.getByRole('link', { name: 'Buzz' }).click();
+    const page1Promise = page.waitForEvent('popup');
+    await page.getByTitle('Help').click();
+    const page1 = await page1Promise;
+  });
+
+
 });
